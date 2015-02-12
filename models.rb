be814b87de9320972ca317a84058e4a88bff1553
Dan Tenenbaum <dtenenba@fhcrc.org>
2015-02-11 16:51:47 -0800

    better way to populate record_id

--- models.rb
+++ models.rb
@@ -48,6 +48,7 @@ class Resource < Sequel::Model
     ## to update all record ids do this:
     ## note, it's slow but hopefully we don't have to do this often
     def self.update_record_ids(force=false)
+
         x = nil
         if force
             x = Resource.where()
@@ -58,46 +59,96 @@ class Resource < Sequel::Model
             if i % 1000 == 0
                 print "."
             end
-            resource.record_id = resource.get_record_id(force)
+            record_id = resource.get_record_id(force)
+            resource.record_id = record_id
+            # puts "record #{resource.id} has record_id #{resource.record_id}"
             resource.save
         end
     end
 
-    def get_record_id(force=false)
-        unless force
-            unless self.record_id.nil?
-                return self.record_id
-            end
-        end
-        max = DB[:resources].max(:record_id)
-        return 1 if (max.nil?)
-        resources = DB[:resources]
-        cands = 
-            resources.where(Sequel.negate(ah_id: \
-                self.ah_id)).where(taxonomyid: self.taxonomyid,
-                genome: self.genome, recipe_id: self.recipe_id)
-        for cand in cands
-            input_sources = InputSource.where(resource_id: cand[:id])
-            if input_sources.count == self.input_sources.count
-                if input_sources.map{|i| i.sourceurl} == \
-                  self.input_sources.map{|i| i.sourceurl}
-                    rdatapaths = Rdatapath.where(resource_id: cand[:id])
-                    if rdatapaths.count == self.rdatapaths.count
-                        if rdatapaths.map{|i| i.rdataclass} == \
-                          self.rdatapaths.map{|i| i.rdataclass}
-                            return cand[:record_id]
-                        end
-                    end
-                end
-            end
-        end
-        max = DB[:resources].max(:record_id) # call again just in case
-        if (max.nil?) # should not happen
+    def self.get_next_record_id()
+        max = Resource.max(:record_id)
+        if (max.nil?) 
             max = 1
         else
             max += 1
         end
         max
+    end
+
+
+    def self.set_record_ids()
+        resources = DB[:resources]
+        h = {}
+        count = 1
+        Resource.all.each do |resource|
+            key = "#{resource.taxonomyid}_#{resource.genome}_#{resource.recipe_id}_"
+            key += "#{resource.input_sources.map{|i| i.sourceurl}.join("_")}"
+            key += "#{resource.rdatapaths.map{|i| i.rdataclass}.join("_")}"
+            h[key] = [] unless h.has_key?(key)
+            h[key] << resource.id
+        end
+        h.each_with_index do |(key, value), index|
+            for id in value
+                r = Resource[id]
+                r.record_id = index
+                r.save
+            end
+        end
+
+    end
+
+    def get_record_id(force=false)
+        # @@resources = Resource.all unless(defined?(@@resources)) 
+        @@input_sources = InputSource.all unless(defined?(@@input_sources)) 
+        @@rdatapaths = Rdatapath.all unless(defined?(@@rdatapaths)) 
+        unless force
+            unless self.record_id.nil?
+                # puts "r0"
+                return self.record_id
+            end
+        end
+        resources = DB[:resources]
+        max = resources.max(:record_id)
+        # puts "r0.5, max is #{max}"
+        return 1 if (max.nil?)
+        # cands = @@resources.find_all do |i|
+        #     i.ah_id != self.ah_id &&
+        #     i.taxonomyid == self.taxonomyid &&
+        #     i.genome == self.genome &&
+        #     i.recipe_id = self.recipe_id
+        # end
+        cands = \
+            resources.where(Sequel.negate(ah_id: \
+                self.ah_id)).where(taxonomyid: self.taxonomyid,
+                genome: self.genome, recipe_id: self.recipe_id)
+        for cand in cands
+            #input_sources = InputSource.where(resource_id: cand[:id])
+            input_sources = @@input_sources.find_all {|i| i.resource_id == self.id}
+            if input_sources.count == self.input_sources.count
+                if input_sources.map{|i| i.sourceurl} == \
+                  self.input_sources.map{|i| i.sourceurl}
+                    #rdatapaths = Rdatapath.where(resource_id: cand[:id])
+                    rdatapaths = @@rdatapaths.find_all {|i| i.resource_id == self.id}
+
+                    if rdatapaths.count == self.rdatapaths.count
+                        if rdatapaths.map{|i| i.rdataclass} == \
+                          self.rdatapaths.map{|i| i.rdataclass}
+                            # puts "r1"
+                            bingo = Resource[cand[:id]]
+                            # puts "bingo: #{bingo.record_id}."
+                            if bingo.record_id.nil?
+                                bingo.record_id = Resource.get_next_record_id
+                                bingo.save
+                            end
+                            return bingo.record_id
+                        end
+                    end
+                end
+            end
+        end
+        # puts "r2"
+        Resource.get_next_record_id
     end
 
     one_to_many :rdatapaths
