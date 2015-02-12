3b47c15858d22da6824e0761dccf0e2b5cdc9b16
Dan Tenenbaum <dtenenba@fhcrc.org>
2015-02-11 16:57:51 -0800

    record_id algo should be working, validation is temporarily disabled

--- models.rb
+++ models.rb
@@ -45,38 +45,7 @@ class Resource < Sequel::Model
         true
     end
 
-    ## to update all record ids do this:
-    ## note, it's slow but hopefully we don't have to do this often
-    def self.update_record_ids(force=false)
-
-        x = nil
-        if force
-            x = Resource.where()
-        else
-            x = Resource.where(record_id: nil)
-        end
-        x.each_with_index do |resource, i|
-            if i % 1000 == 0
-                print "."
-            end
-            record_id = resource.get_record_id(force)
-            resource.record_id = record_id
-            # puts "record #{resource.id} has record_id #{resource.record_id}"
-            resource.save
-        end
-    end
-
-    def self.get_next_record_id()
-        max = Resource.max(:record_id)
-        if (max.nil?) 
-            max = 1
-        else
-            max += 1
-        end
-        max
-    end
-
-
+    ## Note: this will (re-)set all record_ids.
     def self.set_record_ids()
         resources = DB[:resources]
         h = {}
@@ -98,58 +67,45 @@ class Resource < Sequel::Model
 
     end
 
+
     def get_record_id(force=false)
-        # @@resources = Resource.all unless(defined?(@@resources)) 
-        @@input_sources = InputSource.all unless(defined?(@@input_sources)) 
-        @@rdatapaths = Rdatapath.all unless(defined?(@@rdatapaths)) 
         unless force
             unless self.record_id.nil?
-                # puts "r0"
                 return self.record_id
             end
         end
-        resources = DB[:resources]
-        max = resources.max(:record_id)
-        # puts "r0.5, max is #{max}"
+        max = DB[:resources].max(:record_id)
         return 1 if (max.nil?)
-        # cands = @@resources.find_all do |i|
-        #     i.ah_id != self.ah_id &&
-        #     i.taxonomyid == self.taxonomyid &&
-        #     i.genome == self.genome &&
-        #     i.recipe_id = self.recipe_id
-        # end
-        cands = \
+        resources = DB[:resources]
+        cands = 
             resources.where(Sequel.negate(ah_id: \
                 self.ah_id)).where(taxonomyid: self.taxonomyid,
                 genome: self.genome, recipe_id: self.recipe_id)
         for cand in cands
-            #input_sources = InputSource.where(resource_id: cand[:id])
-            input_sources = @@input_sources.find_all {|i| i.resource_id == self.id}
+            input_sources = InputSource.where(resource_id: cand[:id])
             if input_sources.count == self.input_sources.count
                 if input_sources.map{|i| i.sourceurl} == \
                   self.input_sources.map{|i| i.sourceurl}
-                    #rdatapaths = Rdatapath.where(resource_id: cand[:id])
-                    rdatapaths = @@rdatapaths.find_all {|i| i.resource_id == self.id}
-
+                    rdatapaths = Rdatapath.where(resource_id: cand[:id])
                     if rdatapaths.count == self.rdatapaths.count
                         if rdatapaths.map{|i| i.rdataclass} == \
                           self.rdatapaths.map{|i| i.rdataclass}
-                            # puts "r1"
-                            bingo = Resource[cand[:id]]
-                            # puts "bingo: #{bingo.record_id}."
-                            if bingo.record_id.nil?
-                                bingo.record_id = Resource.get_next_record_id
-                                bingo.save
-                            end
-                            return bingo.record_id
+                            return cand[:record_id]
                         end
                     end
                 end
             end
         end
-        # puts "r2"
-        Resource.get_next_record_id
+        max = DB[:resources].max(:record_id) # call again just in case
+        if (max.nil?) # should not happen
+            max = 1
+        else
+            max += 1
+        end
+        max
     end
+
+
 
     one_to_many :rdatapaths
     one_to_many :input_sources
